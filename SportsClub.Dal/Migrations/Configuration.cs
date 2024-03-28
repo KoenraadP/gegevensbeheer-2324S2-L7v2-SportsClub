368b9547a47413d09a58cc31f27d03c30f291ba9
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-03-28 10:48:31 +0100

    Add project files.

---
+++ SportsClub.Dal/Migrations/Configuration.cs
@@ -0,0 +1,34 @@
+﻿namespace SportsClub.Dal.Migrations
+{
+    using SportsClub.Entities;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SportsClub.Dal.SportsClubDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SportsClub.Dal.SportsClubDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            // test members toevoegen aan db
+            context.Members.AddOrUpdate(x => new {x.FirstName, x.LastName},
+                new Member("Vince","Iserniet"),
+                new Member("Moestie","Iserookniet"));
+
+            // test activiteiten toevoegen aan db
+            context.Activities.AddOrUpdate(x => x.Name,
+                new Activity("Voetbal", 30),
+                new Activity("Volleybal", 20));
+        }
+    }
+}
---
+++ SportsClub.Entities/Member.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SportsClub.Entities
+{
+    // sjabloon voor Members table in db
+    public class Member
+    {
+        // memberid --> primary key voor databank
+        // [Key] om aan te duiden dat dit de PK is
+        [Key]
+        public int MemberId { get; set; }
+        [Required(ErrorMessage = "Voornaam mag niet leeg zijn.")]
+        [StringLength(20,MinimumLength = 2, 
+            ErrorMessage = "Lengte voornaam tussen {2} en {1}.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Achternaam mag niet leeg zijn.")]
+        public string LastName { get; set; }
+
+        // property die link legt met Activity
+        // een member kan zich inschrijven voor meerdere activities --> list
+        public List<Activity> Activities { get; set; }
+
+        // constructor om te zorgen dat Members altijd met een
+        // voornaam en achternaam aangemaakt worden
+        public Member(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            // lege list initialiseren
+            Activities = new List<Activity>();
+        }
+
+        public Member()
+        {
+            
+        }
+    }
+}
